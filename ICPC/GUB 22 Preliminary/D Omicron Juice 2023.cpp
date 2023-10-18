4d5bd886397964f91e5bbe4c438823bf65cd48fa
saiyadm99 <81185811+saiyadm99@users.noreply.github.com>
2023-10-18 15:38:14 +0600

    Update D Omicron Juice 2023

--- ICPC/GUB 22 Preliminary/D Omicron Juice 2023.cpp
+++ ICPC/GUB 22 Preliminary/D Omicron Juice 2023.cpp
@@ -13,44 +13,6 @@
 #include <string>
 using namespace std;
 
-int main(){
-  int t;
-  cin >> t;
-  for(int i=1; i<=t; i++){
-    long long a, b, c, k;
-    cin >> a >> b >> c >> k;
-
-    long long aa = a/k;
-    long long bb = b/k;
-    long long cc = c/k;
-
-    long long sum = aa + bb + cc;
-
-    long long am = a%k;
-    long long bm = b%k;
-    long long cm = c%k;
-
-    if(a==b && b == c){
-      cout <<"Case "<<i<<": Peaceful\n";
-    }else{
-      if(am == bm && bm == cm){
-        if((a<k && b<k) || (a<k && c<k) || (b<k && c<k)){
-          cout <<"Case "<<i<<": Fight\n";
-        }else{
-          if(sum%3 == 0){
-          cout <<"Case "<<i<<": Peaceful\n";
-          }else{
-            cout <<"Case "<<i<<": Fight\n";
-          }
-        }
-      }else{
-        cout <<"Case "<<i<<": Fight\n";
-      }
-    }
-  }
-}
-
-
 string sol(){
   int a, b, c, k;
   cin >> a >> b >> c >> k;
@@ -72,3 +34,45 @@ int main(){
     cout << "Case " << i << ": " << sol() <<"\n";
   }
 }
+
+
+// Old Version
+/* 
+  int main(){
+    int t;
+    cin >> t;
+    for(int i=1; i<=t; i++){
+      long long a, b, c, k;
+      cin >> a >> b >> c >> k;
+  
+      long long aa = a/k;
+      long long bb = b/k;
+      long long cc = c/k;
+  
+      long long sum = aa + bb + cc;
+  
+      long long am = a%k;
+      long long bm = b%k;
+      long long cm = c%k;
+  
+      if(a==b && b == c){
+        cout <<"Case "<<i<<": Peaceful\n";
+      }else{
+        if(am == bm && bm == cm){
+          if((a<k && b<k) || (a<k && c<k) || (b<k && c<k)){
+            cout <<"Case "<<i<<": Fight\n";
+          }else{
+            if(sum%3 == 0){
+            cout <<"Case "<<i<<": Peaceful\n";
+            }else{
+              cout <<"Case "<<i<<": Fight\n";
+            }
+          }
+        }else{
+          cout <<"Case "<<i<<": Fight\n";
+        }
+      }
+    }
+  }
+
+*/
